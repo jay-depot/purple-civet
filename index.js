c90adaa01424bc72a5f2322d91a6ff25d3ce3e1f
unleet <unleet.42@gmail.com>
2015-11-21 22:24:45 -0500

    Update API exports

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-module.exports.state = require('./lib/game').state;
+//Extension API
+module.exports.StoryNode          = require('./lib/story_node');
+module.exports.ReactiveDictionary = require('./lib/reactive_dictionary');
 
+
+//Game API
+module.exports.state = require('./lib/game').state;
+module.exports.graph = require('./lib/story_graph');
+module.exports.loadNodes  = require('./lib/data').loadNodes;
