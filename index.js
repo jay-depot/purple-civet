41b90136b513efe7a5237797166d40268de79b99
Jason <jason.depeaux@gmail.com>
2015-11-24 21:26:42 -0500

    Low-level savegame API

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+var path = require('path');
+
 //Extension API
 module.exports.StoryNode          = require('./lib/story_node');
 module.exports.ReactiveDictionary = require('./lib/reactive_dictionary');
 
 
 //Game API
-module.exports.state = require('./lib/game').state;
-module.exports.graph = require('./lib/story_graph');
-module.exports.loadNodes  = require('./lib/data').loadNodes;
+module.exports.state     = require('./lib/game').state;
+module.exports.graph     = require('./lib/story_graph');
+module.exports.loadNodes = require('./lib/data').loadNodes;
+module.exports.savegamne = path.join(path.dirname(module.filename), 'lib', 'frontend', 'savegame.js');
 
 //Event handlers
 require('./lib/keyboard_shortcuts');
